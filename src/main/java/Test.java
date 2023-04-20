bb02142fdfb91333b61144bc9096b534633328cc
LAPTOP-5B2HQHBO\quxiaowei <13898624029@163.com>
2023-04-20 10:53:41 +0800

    测试

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author:quxiaowei
+ * @create:2023-04-20 10:50
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
