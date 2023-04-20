2f9a0c07c2271a4d934e9cc2db50533da36eeb3b
LAPTOP-5B2HQHBO\quxiaowei <13898624029@163.com>
2023-04-20 13:37:56 +0800

    测试

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,5 +7,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
         System.out.println("123");
+        System.out.println("4567");
     }
 }
